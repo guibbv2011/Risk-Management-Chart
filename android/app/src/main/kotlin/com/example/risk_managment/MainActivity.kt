6131ac15dded88cac4a5cca5e52c26955c509878
Guilherme Reinol <guibbv2011@hotmail.com>
2025-05-31 11:00:16 -0300

    Init Risk Managment chart project

---
+++ android/app/src/main/kotlin/com/example/risk_managment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.risk_managment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:signals/signals_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      title: 'Risk Managment',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const MyHomePage(title: 'Risk Managment App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SignalsMixin {
+  // late final counter = createSignal(0);
+  // void _incrementCounter() => counter.value++;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        foregroundColor: Colors.white30,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
